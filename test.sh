0139d7997d656fa71c0a4e0b7f1dd87278848717
harmly <harmly@localhost.localdomain>
2026-02-09 16:34:42 +0000

    Verbose testing added

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ cd test
 cob64 -zUC "use(../test_directives.dir)" "${DIRECTORY_PATH}/src/cobol/GenerateParseDemo.cbl" 
 
 # Run the program
-cobmfurun GenerateParseDemo.so
+cobmfurun -verbose GenerateParseDemo.so
 
 # Remove the DLL
 rm -f GenerateParseDemo.so GenerateParseDemo.o GenerateParseDemo.int GenerateParseDemo.idy
