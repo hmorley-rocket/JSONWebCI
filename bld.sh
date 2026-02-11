442a9dcb73b1c9006af7e9240f883cc66b0d5a7a
harmly <harmly@localhost.localdomain>
2026-02-11 10:01:00 +0000

    updated bld.sh to compile and zip deployable files for containerization

--- bld.sh
+++ bld.sh
@@ -11,4 +11,14 @@ cob64 -x "${DIRECTORY_PATH}/src/cobol/GenerateParseDemo.cbl"
 # Remove object files
 rm -f *.o *.idy *.int
 
+mkdir -p "JSONWebContainer/src"
+
+cp -r "../src/web/${*}" "JSONWebContainer/src"
+cp "../package.json" "JSONWebContainer"
+cp "../package-lock.json" "JSONWebContainer"
+cp "GenerateParseDemo" "JSONWebContainer"
+
+tar czf "JSONWebContainer.tar.gz" "JSONWebContainer"
+rm -r "JSONWebContainer" 
+
 echo "Build complete."
